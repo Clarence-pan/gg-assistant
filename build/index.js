c685c806f0d00683f2de239fcd30b5e2e1773129
Clarence <clarence-pan@foxmail.com>
2016-09-30 17:46:55 +0800

    update

---
+++ build/index.js
@@ -0,0 +1,15 @@
+console.log("Started!")
+
+
+log("Hello!");
+log("typeof nw: " + (typeof nw));
+log("typeof window: " + (typeof window));
+log("typeof require: " + (typeof require));
+log("typeof jQuery: " + (typeof jQuery));
+log("typeof React: " + (typeof React));
+
+function log(msg){
+    document.write("<div>"+msg+"</div>");
+}
+
+
